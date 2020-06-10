aa702f5e9371717c3670afb2d3b392af70e8c17f
KanXxw46 <1kan_andreikkkk@mail.ru>
2020-06-10 18:59:16 +0600

    Dz

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class MyArray
+{
+    int[] arr;
+
+    public MyArray(int size)
+    {
+        arr = new int[size];
+    }
+
+    public int this[int index]
+    {
+        get
+        {
+            return arr[index];
+        }
+    }
+
+    public int this[int index, int number]
+    {
+        set
+        {
+            arr[index] = number * number;
+        }
+    }
+}
